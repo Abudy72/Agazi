053eb665204407ed887674328d931fa53e474d60
Abdella <abudy72@my.yorku.ca>
2025-03-23 01:47:27 -0400

    fixed nested html tages issues

--- components/League/HomePage/LeagueDefaultPage.tsx
+++ components/League/HomePage/LeagueDefaultPage.tsx
@@ -1,5 +1,6 @@
-// pages/leagues.tsx
-import { useRouter } from 'next/navigation'; // Use next/navigation for App Router
+"use client";  // Ensure this component runs on the client
+
+import { useRouter } from 'next/navigation';
 import { ChangeEvent, useEffect, useState } from 'react';
 
 interface League {
@@ -27,7 +28,6 @@ const LeaguesPage = () => {
         } else {
           throw new Error('Invalid data format');
         }
-
         setIsLoading(false);
       } catch (error) {
         console.error('Failed to fetch leagues:', error);
@@ -43,12 +43,16 @@ const LeaguesPage = () => {
   const handleLeagueChange = (event: ChangeEvent<HTMLSelectElement>) => {
     const selectedLeagueId = event.target.value;
     if (selectedLeagueId) {
-      router.push(`/leagues/${selectedLeagueId}`);
+      setIsLoading(true); // Trigger a loading state before navigation
+      setTimeout(() => {
+        router.push(`/leagues/${selectedLeagueId}`);
+      }, 700); // Add a small delay to allow state to update
     }
   };
+  
 
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center p-4">
       <h1 className="text-2xl font-bold mb-4">Choose a League</h1>
       {isLoading ? (
         <p>Loading leagues...</p>
@@ -56,18 +60,17 @@ const LeaguesPage = () => {
         <select
           onChange={handleLeagueChange}
           className="px-4 py-2 border border-black rounded-md shadow-sm focus:outline-none 
-            focus:ring-2 focus:ring-blue-500 bg-lightBlue "
+            focus:ring-2 focus:ring-blue-500 bg-lightBlue text-white"
         >
           <option value="">Select a league</option>
           {leagues.map((league) => (
             <option
-            className="bg-blue text-white hover:bg-gray-700"
-            key={league.league_id}
-            value={league.league_id}
-          >
-            {league.league_name}
-          </option>
-          
+              className="bg-blue text-white hover:bg-gray-700"
+              key={league.league_id}
+              value={league.league_id}
+            >
+              {league.league_name}
+            </option>
           ))}
         </select>
       )}
